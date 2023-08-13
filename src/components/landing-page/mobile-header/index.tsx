bd1cc0cae4365fbe0f5f42a28dd84999d667d214
qgatssdev <chivoomodu@gmail.com>
2023-08-13 21:30:47 +0100

    finalise mobile header

--- src/components/landing-page/mobile-header/index.tsx
+++ src/components/landing-page/mobile-header/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { Twirl as Hamburger } from 'hamburger-react';
+import './styles.scss'
 
 const MobileHeader = () => {
   return (
-    <div>
-        <p></p>
+    <div className='mobile_header_container'>
+      <p>Uche Omodu</p>
+      <Hamburger color='#CCF381' duration={0.8} />
     </div>
-  )
-}
+  );
+};
 
-export default MobileHeader
+export default MobileHeader;
