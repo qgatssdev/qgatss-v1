90424cacacfede375dd89d5c6631c071c7bd9dd4
qgatssdev <chivoomodu@gmail.com>
2023-08-13 21:30:59 +0100

    Changes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,25 @@
 import About from 'components/landing-page/about';
+import Footer from 'components/landing-page/footer';
 import Header from 'components/landing-page/header';
 import Hero from 'components/landing-page/hero';
+import Projects from 'components/landing-page/projects';
 import Quote from 'components/landing-page/quote';
 import Skills from 'components/landing-page/skills';
 import './styles.scss';
 
 const Home = () => {
   return (
-    <div className='home_container'>
-      <Header />
-      <Hero />
-      <About />
-      <Skills />
-      <Quote />
-    </div>
+    <>
+      <div className='home_container'>
+        <Header />
+        <Hero />
+        <About />
+        <Skills />
+        <Quote />
+        <Projects />
+      </div>
+      <Footer />
+    </>
   );
 };
 
