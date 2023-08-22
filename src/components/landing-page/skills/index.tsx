0dd96b7aa47e3c905fea79cc2ce9156fe69ce529
qgatssdev <chivoomodu@gmail.com>
2023-08-22 21:03:06 +0100

    new changes

--- src/components/landing-page/skills/index.tsx
+++ src/components/landing-page/skills/index.tsx
@@ -1,3 +1,4 @@
+import { onGreen, onTransparent } from 'animations/general';
 import Marquee from 'react-fast-marquee';
 import { currentSkills } from './current-skills';
 import './styles.scss';
@@ -5,25 +6,40 @@ import './styles.scss';
 const Skills = () => {
   return (
     <div className='skills_container'>
-      <h1>Skills
-      <img
+      <h1>
+        <span onMouseEnter={onTransparent} onMouseLeave={onGreen}>
+          SKILLS
+        </span>
+        <img
           src={require('../../../assets/Ellipse 6.svg').default}
           alt='eclipse'
           className='skills_eclipse'
         />
       </h1>
       <div className='skills_marquee_container'>
-        <Marquee pauseOnHover>
+        <Marquee>
           <div className='skills_marquee'>
             {currentSkills.map((skill, index) => (
-              <p key={index}>{skill}</p>
+              <p
+                key={index}
+                onMouseEnter={onTransparent}
+                onMouseLeave={onGreen}
+              >
+                {skill}
+              </p>
             ))}
           </div>
         </Marquee>
-        <Marquee pauseOnHover direction='right'>
+        <Marquee direction='right'>
           <div className='skills_marquee'>
             {currentSkills.reverse().map((skill, index) => (
-              <p key={index}>{skill}</p>
+              <p
+                key={index}
+                onMouseEnter={onTransparent}
+                onMouseLeave={onGreen}
+              >
+                {skill}
+              </p>
             ))}
           </div>
         </Marquee>
